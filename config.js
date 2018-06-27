c99bb356c849ce4ef8ad4f88d8dbc9c471281444
Robert Kalcik <robert_kck@hotmail.com>
2018-06-27 20:45:03 +0200

    undo changes

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const twitterApp = {
-  consumer_key: process.env.ayGzE6WOvZf5vLGTd5lrLpMes,
-  consumer_secret: process.env.HscCoFzBB29nABNc69OIuLXWK6tigib9KPkfbP4gBxV7pvsbyo,
-  access_token: process.env.3929637676-UxwEFtaVR9dTLP5ZRyDdQMMOs0jwxyivsyFcuAZ,
-  access_token_secret: process.env.fnEQ7JMF5yHZ7IfrYYpW9dxh4okXW7aCpvEpwERAd70V3,
+    consumer_key: process.env.CONSUMER_KEY,
+	consumer_secret: process.env.CONSUMER_SECRET,
+	access_token: process.env.ACCESS_TOKEN,
+	access_token_secret: process.env.ACCESS_TOKEN_SECRET,
   timeout_ms: 60 * 1000 // optional HTTP request timeout to apply to all requests.
 };
 

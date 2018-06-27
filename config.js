81c442bd122fd817ec1efafa5afa454fe422043b
Robert Kalcik <robert_kck@hotmail.com>
2018-06-27 20:48:33 +0200

    Whats wrong

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const twitterApp = {
-    consumer_key: process.env.CONSUMER_KEY,
+	consumer_key: process.env.CONSUMER_KEY,
 	consumer_secret: process.env.CONSUMER_SECRET,
 	access_token: process.env.ACCESS_TOKEN,
 	access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-  timeout_ms: 60 * 1000 // optional HTTP request timeout to apply to all requests.
+	timeout_ms: 60 * 1000 // optional HTTP request timeout to apply to all requests.
 };
 
 module.exports = {
